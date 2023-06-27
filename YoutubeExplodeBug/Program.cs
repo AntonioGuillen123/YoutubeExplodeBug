2c9f4a07b264dc60dff060b4600cbb759de50ab0
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-06-27 13:38:14 +0200

    first commit

---
+++ YoutubeExplodeBug/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeExplodeBug
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			string url = "https://www.youtube.com/watch?v=k59E7T0H-Us&ab_channel=EurovisionSongContest";
+
+			YoutubeClient client = new YoutubeClient();
+			var streamManifest = await client.Videos.Streams.GetManifestAsync(url);
+			var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+			using Process ffmpeg = CreateStream(streamInfo.Url);
+			using Stream audioStream = ffmpeg.StandardOutput.BaseStream;
+
+			byte[] buffer = new byte[48000 * 2 * 16];
+
+			while (true)
+			{
+				await audioStream.ReadAsync(buffer);
+				await Task.Delay(100);
+			}
+		}
+
+		private static Process CreateStream(string path) => Process.Start(new ProcessStartInfo
+		{ 
+			FileName = "ffmpeg",
+			Arguments = $"-i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+			UseShellExecute = false,
+			RedirectStandardOutput = true,
+		});
+	}
+}
